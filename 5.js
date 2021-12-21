f8202d11ff45589cdd89c1f9d6de421ae5aa98f5
Salma Noe <sattari2101@gmail.com>
2021-12-20 21:17:22 -0800

    commit

---
+++ 5.js
@@ -0,0 +1,15 @@
+function myFunction(y1, y2, ...y3) {
+    const [x1, ...[result]] = y3;
+
+    console.log(result);
+}
+
+const myArray = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+myFunction(...myArray);
+
+
+// why is the output 'lizard' ?
+myFunction('rock', 'paper', 'scissors', 'lizard', 'spock');
+const [a, ...[X]] = ['scissors', 'lizard', 'spock'];
+// apparently there is a difference between ...[1, 2, 3] and ...1
