3d8814267f2073fca81abd132de66be46ae01502
Salma Noe <sattari2101@gmail.com>
2021-12-20 19:54:21 -0800

    2.js complete

---
+++ 2.js
@@ -0,0 +1,12 @@
+const mySet = new Set();
+
+mySet.add(1);
+mySet.add(1);
+mySet.add('a');
+mySet.add('a');
+mySet.add(undefined);
+mySet.add(undefined);
+mySet.add({prop: true});
+mySet.add({prop: true});
+
+console.log('Size: ', mySet);
