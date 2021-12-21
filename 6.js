bd77362acfb593f185ddc141aa32e7c1ba2f3e07
Salma Noe <sattari2101@gmail.com>
2021-12-20 21:22:01 -0800

    commit

---
+++ 6.js
@@ -0,0 +1,9 @@
+// const double = x => x * 2; 
+
+// function double(x) { return x * 2; };
+
+// function double = () => return x * 2; // <- this is wrong
+
+// const double = function(x) { return x * 2; };
+
+console.log(double(2));
