8bd181658d531340cbe8bfd8de533b718f813922
Salma Noe <sattari2101@gmail.com>
2022-01-05 18:27:34 -0800

    commit

--- variety.js
+++ variety.js
@@ -11,6 +11,18 @@
 // }
 // console.log(sayHello.prototype);
 
-const foo = [1, 2, 3];
-const [n] = [foo];
-console.log(n);
+// const foo = [1, 2, 3];
+// const [n] = [foo];
+// console.log(n);
+
+var v = 1;
+var f1 = function () {
+  console.log(v);
+};
+
+var f2 = function () {
+  var v = 2;
+  f1();
+};
+
+f2();
