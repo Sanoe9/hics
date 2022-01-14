24e4d0f384e19dc8725134476ec5cb4bb46d2da1
Salma Noe <sattari2101@gmail.com>
2022-01-13 19:49:11 -0800

    o(N) solution

--- findMissingNumber.js
+++ findMissingNumber.js
@@ -5,17 +5,31 @@ var arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
 var upperBound = 9;
 var lowerBound = 1;
 
+// const findMissingNumber = (arrayOfIntegers, upperBound, lowerBound) => {
+//     // BRUTE FORCE SOLUTION
+
+//     arrayOfIntegers.sort((a, b) => a - b);
+    
+//     for (let i = 0; i < arrayOfIntegers.length; i++) {
+//         const integer = arrayOfIntegers[i];
+//         if (lowerBound !== integer) {
+//             return lowerBound;
+//         } 
+//         lowerBound++;
+//     }
+// };
+
 const findMissingNumber = (arrayOfIntegers, upperBound, lowerBound) => {
-    // BRUTE FORCE SOLUTION
+    // O(N) SOLUTION
     
-    arrayOfIntegers.sort((a, b) => a - b);
-    
+    // sum the array and create a sum variable which will be the sum from upperbound and lowerbound and return that variable sum - the array sum
+    let arraySum = 0;
+    const upperLowerSum = (upperBound * (upperBound + 1)) / 2;
     for (let i = 0; i < arrayOfIntegers.length; i++) {
-        const integer = arrayOfIntegers[i];
-        if (lowerBound !== integer) {
-            return lowerBound;
-        } 
-        lowerBound++;
+        arraySum += arrayOfIntegers[i];
     }
+
+    return upperLowerSum - arraySum;
 };
+
 console.log(findMissingNumber(arrayOfIntegers, upperBound, lowerBound)); // 8
