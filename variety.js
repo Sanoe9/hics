18105cdf1bb68f02fe0392af63fe43a7ac2f3927
Salma Noe <sattari2101@gmail.com>
2022-01-14 19:46:30 -0800

    more

--- variety.js
+++ variety.js
@@ -1,15 +1,15 @@
-// 'use strict';
-// function logThis() {
-//   this.desc="logger";
-//   console.log(this);
-// }
+'use strict';
+function logThis() {
+  this.desc="logger";
+  console.log(this);
+}
 
-// logThis();
+logThis();
 
-// function sayHello() {
-//     console.log('hello');
-// }
-// console.log(sayHello.prototype);
+function sayHello() {
+    console.log('hello');
+}
+console.log(sayHello.prototype);
 
 const foo = [1, 2, 3];
 const [n] = [foo];
