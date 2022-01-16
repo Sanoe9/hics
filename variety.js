830c399b695b016101615ad1a9f8fd1ee0cb3312
Salma Noe <sattari2101@gmail.com>
2022-01-16 10:01:31 -0800

    commit

--- variety.js
+++ variety.js
@@ -1,28 +1,44 @@
-'use strict';
-function logThis() {
-  this.desc="logger";
-  console.log(this);
-}
+// 'use strict';
+// function logThis() {
+//   this.desc="logger";
+//   console.log(this);
+// }
 
-logThis();
+// logThis();
 
-function sayHello() {
-    console.log('hello');
-}
-console.log(sayHello.prototype);
+// function sayHello() {
+//     console.log('hello');
+// }
+// console.log(sayHello.prototype);
 
-const foo = [1, 2, 3];
-const [n] = [foo];
-console.log(n);
+// const foo = [1, 2, 3];
+// const [n] = [foo];
+// console.log(n);
 
-var v = 1;
-var f1 = function () {
-  console.log(v);
-};
+// var v = 1;
+// var f1 = function () {
+//   console.log(v);
+// };
 
-var f2 = function () {
-  var v = 2;
-  f1();
-};
+// var f2 = function () {
+//   var v = 2;
+//   f1();
+// };
 
-f2();
+// f2();
+
+// var x = { foo : 1};
+// var output = (function() {
+//   delete x.foo;
+//   return x.foo;
+// })();
+
+// console.log(output);
+
+var x = 1;
+var output = (function() {
+  delete x;
+  return x;
+})();
+
+console.log(output);
