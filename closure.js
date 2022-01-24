ba7e439145d6a8b00e8cfb7ed4c7a43cb602574d
Salma Noe <sattari2101@gmail.com>
2022-01-23 20:43:59 -0800

    commit

--- closure.js
+++ closure.js
@@ -1,8 +1,8 @@
 // Write a function that would allow you to do this.
 
-// var addSix = createBase(6);
-// addSix(10); // returns 16
-// addSix(21); // returns 27
+var addSix = createBase(6);
+addSix(10); // returns 16
+addSix(21); // returns 27
 
 const createBase = (number) => {
     return (numero) => {
