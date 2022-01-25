81dfdee24f64239e4e2541c1ca01feec0d9bc7d9
Salma Noe <sattari2101@gmail.com>
2022-01-24 21:53:32 -0800

    more js

--- 3.js
+++ 3.js
@@ -1,13 +1,47 @@
-let x = 'fog';
+// let x = 'fog';
 
-function first() {
-    console.log(x);
-};
+// function first() {
+//     console.log(x);
+// };
 
-x = 'dog';
-function second() {
-    let x = 'log';
-    first();
-};
+// x = 'dog';
+// function second() {
+//     let x = 'log';
+//     first();
+// };
 
-second();
+// second();
+
+
+// const a = 0;
+// const b = '';
+// const c = [false]
+
+// const outcome = !!(a || b || c || d)
+
+// const sparseArray = [,1,,2,,3,,4]
+
+// let sum = 0;
+// sparseArray.forEach(val => {
+//     sum += val;
+// })
+
+// console.log(sum)
+
+function* gen1() {
+    yield 'a'
+    yield 'b'
+    yield 'c'
+
+}
+
+function* gen2() {
+    yield* gen1()
+    yield* [1, 2, 3]
+    return 4
+
+}
+
+for (const element of gen2()) {
+    console.log(element)
+}
